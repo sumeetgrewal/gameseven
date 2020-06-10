349711aa0eb358eb9367c50d54d3c1663256aeb2
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2020-06-09 22:05:53 -0700

    GS-6 create app component

---
+++ client/src/components/JoinGame.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class JoinGame extends React.Component {
+  render() {
+    return (
+      <div className="JoinGame my-4">
+        Test
+      </div>
+    );
+  } 
+}
+
+export default JoinGame;
