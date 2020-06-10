7d9cc5279ecc132a238a93d88066d6301dd8f4fd
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2020-06-09 23:43:40 -0700

    GS-6 Join Game Form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 import JoinGame from './components/JoinGame';
 
-class App extends React.Component {
+interface MyState {
+  gameConnected: boolean
+} 
+
+interface MyProps {
+
+}
+class App extends React.Component<MyProps, MyState> {
+  constructor(props: MyProps) {
+    super(props);
+    this.state = {
+        gameConnected: false
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <JoinGame />
+          <JoinGame 
+            gameConnected={this.state.gameConnected}
+          />
       </div>
     );
   } 
--- client/src/components/JoinGame.tsx
+++ client/src/components/JoinGame.tsx
@@ -1,10 +1,67 @@
 import React from 'react';
 
-class JoinGame extends React.Component {
+interface JoinGameProps {
+  gameConnected: boolean,
+}
+
+interface JoinGameState {
+  connectionPending: boolean,
+}
+class JoinGame extends React.Component<JoinGameProps, JoinGameState> {
+  constructor(props: JoinGameProps) {
+    super(props);
+    this.state = {
+      connectionPending: false,
+    }
+  }
+
+  joinGame(event: any) {
+    event.preventDefault();
+    console.log("Send Game Connection Request")
+    console.log(event.target);
+    this.setState({connectionPending: true},
+      () => {
+        this.sendConnectionRequest()
+        .then(() => {
+          console.log("Connected");
+          this.setState({connectionPending: false});
+        }
+      )}
+    )
+  }
+
+  sendConnectionRequest(): Promise<void> {
+    return new Promise((resolve) => {
+      // TODO Send Request
+      console.log("Waiting for connection")
+      setTimeout(() => resolve(), 2000)
+    })
+  }
+
   render() {
+    const connectionPending = this.state.connectionPending
     return (
-      <div className="JoinGame my-4">
-        Test
+      <div className="container d-flex align-items-center text-center justify-content-center" style={{height: '100vh'}}>
+          <div style={{display: (connectionPending ? "none" : "initial")}}>
+            <form onSubmit={(e) => this.joinGame(e)}>
+              <div className="dialog"> 
+                <label>
+                  <h5 className="dialog-label">ENTER YOUR NAME</h5>
+                  <input id="name-prompt" />
+                </label>
+                <label> 
+                  <h5 className="dialog-label">ENTER PASSWORD</h5>
+                  <input type="password" id="password-prompt" />
+                </label>
+              </div>
+              <button type="submit" className="btn submit-btn">JOIN GAME</button>
+            </form>
+          </div>
+        {connectionPending && 
+          <div>
+            <h3> CONNECTING TO GAME . . . </h3>
+          </div>
+        }
       </div>
     );
   } 
