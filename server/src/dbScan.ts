6665f516bff5b456c4db2ac868ff1b1761f8a967
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2020-06-16 22:07:29 -0700

    GS-5 setup table scan

---
+++ server/src/dbScan.ts
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({region: 'us-west-2'});
+let ddb = new AWS.DynamoDB();
+
+// Print credentials to confirm they are correctly configured
+// The location of the shared credentials file 
+//    on Linux, Unix, and macOS: ~/.aws/credentials
+//    on Windows: C:\Users\USER_NAME\.aws\credentials
+// AWS.config.getCredentials(function(err: any) {
+//     if (err) console.log(err.stack);
+//     else {
+//       console.log("Access key:", AWS.config.credentials.accessKeyId
+//       );
+//     }
+//   });
+
+function tableScan(tableName: string): void {
+    let params = {
+        TableName: tableName
+    };
+    ddb.scan(params, function(err: Error, data: any) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success: Retrieved " + data.Items.length + " entries from " + tableName);
+        }
+    });
+}
+
+exports.tableScan = tableScan;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,41 +1,17 @@
 import express from 'express';
+const dbScan = require('./dbScan')
 
 const app = express();
 const port = process.env.PORT || '8000';
-const AWS = require("aws-sdk");
-
-// Print credentials to confirm they are correctly configured
-// The location of the shared credentials file 
-//    on Linux, Unix, and macOS: ~/.aws/credentials
-//    on Windows: C:\Users\USER_NAME\.aws\credentials
-AWS.config.getCredentials(function(err: any) {
-  if (err) console.log(err.stack);
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId
-    );
-  }
-});
-AWS.config.update({region: 'us-west-2'});
-
-var ddb = new AWS.DynamoDB();
-var params = {
-  ProjectionExpression: 'CARD_ID',
-  TableName: 'CARDS'
-};
-
-ddb.scan(params, function(err: Error, data: any) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.Items);
-    // data.Items.forEach(function(element: any, index: any, array: any) {
-    //   console.log(element.Name.s);
-    // });
-  }
-});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+})
+
+app.get('/load', (req, res) => {
+  dbScan.tableScan('CARDS');
+  dbScan.tableScan('BOARDS');
+  res.send('Loaded Tables');
 })
 
 app.listen(port, err => {
