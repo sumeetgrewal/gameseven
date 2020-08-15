42f6da010ad932825b8f6b865cdd5bbdf209b3f3
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2020-08-15 13:00:42 -0700

    GS-43 prod-build script

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 const compression = require('compression');
 const morgan = require('morgan');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || '8000';
+
+// Serve frontend when in production
+app.use(express.static(path.join(__dirname, '/../client/build')));
 
 app.use(compression({ filter: shouldCompress }))
 app.use(express.json());
@@ -22,6 +25,10 @@ const gamePlayRouter = require('./routes/play');
 
 app.use('/game', gameSetupRouter);
 app.use('/game/play', gamePlayRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+});
 
 app.listen(port, err => {
   if (err) return console.error(err);
