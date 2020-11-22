e5887154061e0e08fa6fb015b415a5d66ff17b66
sumeetgrewal <sumeetsinghgrewal@gmail.com>
2020-11-22 00:30:50 -0800

    GS-56 Fixed purchase options issues

--- server/src/tests/gameplay.test.ts
+++ server/src/tests/gameplay.test.ts
@@ -55,42 +55,251 @@ beforeAll(() => {
     })
 }, )
 
-test('Game setup', () => {
-    expect(sampleGame.gameData.playerData['A'].coins === 3);
-})
+describe('Purchase Options', () => {
+    let left: Player,
+        player: Player, 
+        right: Player;
 
+    beforeAll(() => {
+        left = sampleGame.gameData.playerData['E'];
+        player = sampleGame.gameData.playerData['F'];
+        right = sampleGame.gameData.playerData['G'];
+        
+        player.coins = 100;
+        player.playerLeft = 'E';
+        player.playerRight = 'G';
 
-test('A selects a card', () => {
-    A.selectCard('1', 0, emptyPO)
-    expect(A.cards.length).toEqual(1);
-})
+        left.playerRight = 'F';
+        right.playerLeft = 'F';
 
-test('A selects resource cards', () => {
-    [3, 9, 10].forEach((cardID: number) => A.selectCard(cardID.toString(), 0, emptyPO))
-    expect(A.resources.wood).toEqual(1);
-    expect(A.resources.stone).toEqual(1);
-    expect(A.optionalResources).toEqual([
-        [[1, 'WOOD'], [1, 'CLAY']],
-        [[1, 'STONE'], [1, 'CLAY']]
-    ])
-})
-
-test('B selects cards: [ 12, 5, 19, 42, 48, 97, 62, 95, 54, 127, 146, 133, 101, 110 ]', () => {
-    B.coins = 9;
-    B.stagesBuilt = 2;
-    [ 12, 5, 19, 42, 48, 97, 62, 95, 54, 127, 146, 133, 101, 110 ].map((cardId: number) => {
-        B.selectCard(cardId.toString(), 0, {purchaseLeft: [], purchaseRight: [], costLeft: 0, costRight: 0});
     })
-    expect(gp.calculatePoints(B)).toEqual(44)
+    
+    test('Two equal purchase options', () => {
+        // Need 1 Papyrus, 1 Wood
+        // Left has 1 Papyrus, 1 Wood
+        // Right has 1 Papyrus, 1 Wood
+
+        left.resources = new ResourceList(1);
+        right.resources = new ResourceList(1);
+
+        // ResourceCost = 1 Wood and 1 Papyrus
+        const result = player.canBuild('97');
+
+        expect(result.purchaseOptions.length).toEqual(3);
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({   
+                    purchaseRight: [[1, 'wood'], [1, 'papyrus']],
+                    purchaseLeft:  [],
+                    costRight: 4,
+                    costLeft: 0,              
+                })
+            ])
+        )
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({   
+                    purchaseRight:  [],
+                    purchaseLeft: [[1, 'wood'], [1, 'papyrus']],
+                    costRight: 0,
+                    costLeft: 4,          
+                })
+            ])
+        )
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({   
+                    costRight: 2,
+                    costLeft: 2,          
+                })
+            ])
+        )
+    })
+
+    test('Left has more resources, two different options', () => {
+        right.resources.stone = 1;
+        left.resources.stone = 3;
+        
+        // ResourceCost = 3 Stone
+        const result = player.canBuild('82')
+        
+        expect(result.purchaseOptions.length).toEqual(2);
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({   
+                    purchaseRight: [[1, 'stone']],
+                    purchaseLeft:  [[2, 'stone']],
+                    costRight: 2,
+                    costLeft: 4,              
+                })
+            ])
+        )
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({   
+                    purchaseRight: [],
+                    purchaseLeft:  [[3, 'stone']],
+                    costRight: 0,
+                    costLeft: 6,              
+                })
+            ])
+        )
+        
+
+    })
+
+    test('Purchase with resource discount', () => {
+        // Build East Trading Post (Discount to right player)
+        player.selectCard('32', 0, emptyPO);
+
+        right.resources.stone = 1;
+        left.resources.stone = 3;
+
+        // ResourceCost = 3 Stone
+        const result = player.canBuild('82')
+        
+        expect(result.purchaseOptions.length).toEqual(1);
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({   
+                    purchaseRight: [[1, 'stone']],
+                    purchaseLeft:  [[2, 'stone']],
+                    costRight: 1,
+                    costLeft: 4,              
+                })
+            ])
+        )
+    })
+
+    test('Purchase with glass/papyrus/loom discount', () => {
+        // Build MarketPlace (Discount to both players)
+        player.selectCard('37', 0, emptyPO);
+
+        right.resources = new ResourceList(1);
+        left.resources = new ResourceList(1);
+        right.resources.loom = 0;
+        left.resources.papyrus = 0;
+
+        // ResourceCost = 1 Wood, 1 Papyrus, 1 Loom
+        const result = player.canBuild('137');
+
+        expect(result.purchaseOptions.length).toEqual(1);
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({   
+                    purchaseRight: [[1, 'wood'], [1, 'papyrus']],
+                    purchaseLeft:  [[1, 'loom']],
+                    costRight: 2,
+                    costLeft: 1,              
+                })
+            ])
+        )
+
+    })
+
+    test('Optional Resource Purchase', () => {
+        right.resources = new ResourceList(0);
+        left.resources = new ResourceList(0);
+
+        // Optional resource = 1 wood or 1 stone
+        right.selectCard('12', 0, emptyPO);
+        left.selectCard('12', 0, emptyPO);
+        // 2 Stone
+        left.selectCard('52', 0, emptyPO);
+
+        // ResourceCost = 3 Stone
+        const result = player.canBuild('82')
+
+        expect(result.purchaseOptions.length).toEqual(1);
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({   
+                    purchaseRight: [[1, 'stone']],
+                    purchaseLeft:  [[2, 'stone']],
+                    costRight: 1,
+                    costLeft: 4,              
+                })
+            ])
+        )
+    })
+
+    test("Large Purchase - Palace", () => {
+        right.resources = new ResourceList(0);
+        left.resources = new ResourceList(0);
+
+        // Optional resource = 1 wood or 1 stone
+        right.selectCard('12', 0, emptyPO);
+        left.selectCard('12', 0, emptyPO);
+        // 2 Stone
+        left.selectCard('52', 0, emptyPO);
+        // Optional resource = 1 stone or 1 ore
+        left.selectCard('14', 0, emptyPO);
+        // 1 Press
+        right.selectCard('19',0, emptyPO);
+        // 1 Glass
+        left.selectCard('18', 0, emptyPO);
+        // FORUM
+        player.selectCard('72', 0, emptyPO);
+        // CARAVANSERY 
+        player.selectCard('75', 0, emptyPO);
+
+        // ResourceCost = 1 of everything (PALACE)
+        const result = player.canBuild('106')
+        console.log(result);
+        console.log(result.purchaseOptions[0].purchaseLeft)
+        console.log(result.purchaseOptions[0].purchaseRight)
+
+        expect(result.purchaseOptions.length).toEqual(1);
+
+        expect(result.purchaseOptions).toEqual( 
+            expect.arrayContaining([      
+                expect.objectContaining({  
+                    purchaseRight: expect.arrayContaining(
+                        [[1, 'wood'], [1, 'papyrus']] 
+                    ),
+                    purchaseLeft: expect.arrayContaining(
+                        [[1, 'stone'], [1, 'ore'], [1, 'glass']]
+                    ),
+                    costRight: 2,
+                    costLeft: 5,              
+                })
+            ])
+        )
+    })
+
 })
 
-// test ('B selects cards: [ 12, 5, 19, 42, 48, 97, 62, 95, 54, 127, 146, 133, 101, 110 ]', () => {
-//     [ 12, 5, 19, 42, 48, 97, 62, 95, 54, 127, 146, 133, 101, 110 ].map((cardId: number) => {
-//         A.selectCard(cardId.toString(), 0, {purchaseLeft: [], purchaseRight: [], costLeft: 0, costRight: 0});
-//     })
-//     // expect(A.score === ??? )
-// })
 
+describe('Smoke Tests', () => {
+
+    test('Game setup', () => {
+        expect(sampleGame.gameData.playerData['A'].coins === 3);
+    })    
+    
+    test('A selects a card', () => {
+        A.selectCard('1', 0, emptyPO)
+        expect(A.cards.length).toEqual(1);
+    })
+    
+    test('A selects resource cards', () => {
+        [3, 9, 10].forEach((cardID: number) => A.selectCard(cardID.toString(), 0, emptyPO))
+        expect(A.resources.wood).toEqual(1);
+        expect(A.resources.stone).toEqual(1);
+        expect(A.optionalResources).toEqual([
+            [[1, 'WOOD'], [1, 'CLAY']],
+            [[1, 'STONE'], [1, 'CLAY']]
+        ])
+    })
+    
+    test('B selects cards: [ 12, 5, 19, 42, 48, 97, 62, 95, 54, 127, 146, 133, 101, 110 ]', () => {
+        B.coins = 9;
+        B.stagesBuilt = 2;
+        [ 12, 5, 19, 42, 48, 97, 62, 95, 54, 127, 146, 133, 101, 110 ].map((cardId: number) => {
+            B.selectCard(cardId.toString(), 0, {purchaseLeft: [], purchaseRight: [], costLeft: 0, costRight: 0});
+        })
+        expect(gp.calculatePoints(B)).toEqual(33);
+    })
+})
 
 
 describe('Calculate Science', () => {
